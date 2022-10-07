3d331ee6dbf55b74d7493f54a081853c5ce95027
Buffalodude101 <haydenhart@mail.weber.edu>
2022-10-06 18:10:39 -0600

    init repo

---
+++ numbers.py
@@ -0,0 +1,17 @@
+import random
+
+my_int = 1923
+my_int2 = 1978
+my_random_year = random.randint(1974, 2022)
+print(my_random_year)
+
+time_passed = (my_random_year - my_int)
+print(time_passed)
+
+apple_price = 1.79
+orange_price = 1.25
+banana_price = 2
+banana_pricefloat = float(banana_price)
+
+bill = (apple_price + orange_price + banana_price)
+print(bill)
